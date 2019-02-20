6ad3806762c5f42e8923659f076ea57580a0bfc4
crycetruly <crycetruly@gmail.com>
2019-02-20 14:15:28 +0300

    Added redirect url to food create

--- foods/models.py
+++ foods/models.py
@@ -4,6 +4,10 @@ class Nutrients(models.Model):
     name=models.CharField(max_length=255,null=True)
     description=models.CharField(max_length=500)
     nutrients=models.CharField(max_length=500)
+    
+
+    def get_absolute_url(self):
+        return u'/' 
 
     def __str__(self):
         return self.name+" "+self.nutrients
--- foods/urls.py
+++ foods/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path,include
 from .views import index,NutrientsList,NutrientsDetail,create,FoodCreate
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import reverse
 
 
 
 urlpatterns=[
     path('',index,name='home'),
-     path('fcreate',FoodCreate.as_view(),name='fcreate'),
+    path('fcreate',FoodCreate.as_view(),name='fcreate'),
     path('api/nutrients/',NutrientsList.as_view()),
     path("api/nutrients/<int:pk>/", NutrientsDetail.as_view(), name="nutrient_detail")
 ]
